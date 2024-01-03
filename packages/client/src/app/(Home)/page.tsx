53ca1afc2bb0ae9926b9b85b7d556ad99c4adf31
dchoi315 <dchoi315@umd.edu>
2024-01-02 19:55:03 -0500

    frontend barebones created

--- packages/client/src/app/(Home)/page.tsx
+++ packages/client/src/app/(Home)/page.tsx
@@ -1,25 +1,32 @@
 import Image from "next/image"
 
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+import { Separator } from "@/components/ui/separator"
 
 export default function Home() {
 	return (
-		<div className="h-[calc(100vh-64px)] lg:max-w-screen-lg mx-auto bg-amber-700">
-			<div className="m-auto flex flex-col w-1/2 lg:flex-row lg:w-3/4 ">
-				<Tabs defaultValue="public" className="w-full lg:w-1/2">
-					<TabsList className="grid w-full grid-cols-2">
-						<TabsTrigger value="public">Public</TabsTrigger>
-						<TabsTrigger value="private">Private</TabsTrigger>
-					</TabsList>
-					<TabsContent value="public">
-						<div className="min-h-64 bg-rose-300">
-							Make changes to your account here.
-						</div>
-					</TabsContent>
-					<TabsContent value="private">Change your password here.</TabsContent>
-				</Tabs>
+		<div className="flex justify-center pt-20 h-[calc(100vh-64px)] lg:max-w-screen-lg mx-auto ">
+			<div className="flex flex-col gap-6 h-2/3 w-2/3 lg:flex-row lg:w-3/4 ">
+				<div className="p-6 rounded flex flex-col justify-center items-center gap-6 w-full lg:w-1/2 bg-fc-blue-dark">
+					<Input className="w-2/3" placeholder="Nickname" />
 
-				<div className="w-full lg:w-1/2 bg-green-50 min-h-64">How to Play</div>
+					<Button className="w-24">Start</Button>
+
+					<Separator className="bg-fc-accent px-4" />
+					{/* <div className="flex w-full">
+						<Separator />
+						<span> OR </span>
+						<Separator />
+					</div> */}
+
+					<Button className="w-fit">Create Private Game</Button>
+				</div>
+
+				<div className="p-6 rounded w-full lg:w-1/2 bg-fc-blue-dark">
+					How to Play
+				</div>
 			</div>
 		</div>
 	)
