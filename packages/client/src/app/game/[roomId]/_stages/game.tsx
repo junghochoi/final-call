3f1eb91d81ad0aac8cedc91780438d29d95391ee
dchoi315 <dchoi315@umd.edu>
2024-01-14 21:31:16 -0500

    feat: player seatigns finished

--- packages/client/src/app/game/[roomId]/_stages/game.tsx
+++ packages/client/src/app/game/[roomId]/_stages/game.tsx
@@ -3,29 +3,26 @@ import { ActionBar } from "./_components/actionBar"
 export const Game = () => {
 	return (
 		<div className="bg-green-200 h-screen max-w-screen-lg mx-auto relative overscroll-none">
-			<div className="relative">
-				<div className="absolute w-20 h-14 bg-blue-300 left-[calc(50%-2.5rem)] top-[0.5rem]">
+			<div className="relative h-[calc(100%-6rem)]">
+				<div className="absolute w-20 h-14 md:w-28 md:h-20 bg-blue-300 left-[calc(50%-2.5rem)] top-[0.5rem]">
 					<p>Player Number 4</p>
 				</div>
-				<div className="absolute w-20 h-14 bg-blue-300 left-[0.5rem] top-1/3]">
+				<div className="absolute w-20 h-14 md:w-28 md:h-20 bg-blue-300 left-[0.5rem] top-[20%]">
 					<p>Player Number 3</p>
 				</div>
 
-				<div className="absolute w-20 h-14 bg-blue-300 right-[0.5rem] top-[6rem]">
+				<div className="absolute w-20 h-14 md:w-28 md:h-20 bg-blue-300 right-[0.5rem] bottom-[20%]">
 					<p>Player Number 2</p>
 				</div>
-				<div className="absolute w-20 h-14 bg-blue-300 left-[0.5rem] top-[24rem]">
+				<div className="absolute w-20 h-14 md:w-28 md:h-20 bg-blue-300 left-[0.5rem] bottom-[20%]">
 					<p>Player Number 1</p>
 				</div>
-				<div className="absolute w-28 h-20 bg-blue-300 left-[calc(50%-2.5rem)] top-[36rem]">
+				<div className="absolute w-20 h-14 md:w-28 md:h-20 bg-blue-300 left-[calc(50%-2.5rem)] bottom-[0.5rem]">
 					<p>Player Number 5</p>
 				</div>
-				{/* <div className="absolute w-28 h-20 bg-blue-300 left-[9.2rem]">
-					<p>Player Number 4</p>
+				<div className="absolute w-20 h-14 md:w-28 md:h-20 bg-blue-300 right-[0.5rem] top-[20%]">
+					<p>Player Number 6</p>
 				</div>
-				<div className="absolute w-28 h-20 bg-blue-300 left-[9.2rem]">
-					<p>Player Number 4</p>
-				</div> */}
 			</div>
 			<ActionBar />
 		</div>
