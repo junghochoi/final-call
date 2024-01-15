33c759caffcc97eb4150a7e38e20723aa554925e
dchoi315 <dchoi315@umd.edu>
2024-01-14 20:58:13 -0500

    feat: player seats in progress

--- packages/client/src/app/game/[roomId]/_stages/game.tsx
+++ packages/client/src/app/game/[roomId]/_stages/game.tsx
@@ -4,18 +4,22 @@ export const Game = () => {
 	return (
 		<div className="bg-green-200 h-screen max-w-screen-lg mx-auto relative overscroll-none">
 			<div className="relative">
-				<div className="absolute w-28 h-20 bg-blue-300 left-[9.2rem] top-[0.5rem]">
+				<div className="absolute w-20 h-14 bg-blue-300 left-[calc(50%-2.5rem)] top-[0.5rem]">
 					<p>Player Number 4</p>
 				</div>
-				<div className="absolute w-28 h-20 bg-blue-300 left-[0.5rem] top-[]">
+				<div className="absolute w-20 h-14 bg-blue-300 left-[0.5rem] top-1/3]">
 					<p>Player Number 3</p>
 				</div>
-				<div className="absolute w-28 h-20 bg-blue-300 left-[9.2rem]">
+
+				<div className="absolute w-20 h-14 bg-blue-300 right-[0.5rem] top-[6rem]">
 					<p>Player Number 2</p>
 				</div>
-				{/* <div className="absolute w-28 h-20 bg-blue-300 left-[9.2rem] top-[41rem]">
+				<div className="absolute w-20 h-14 bg-blue-300 left-[0.5rem] top-[24rem]">
 					<p>Player Number 1</p>
-				</div> */}
+				</div>
+				<div className="absolute w-28 h-20 bg-blue-300 left-[calc(50%-2.5rem)] top-[36rem]">
+					<p>Player Number 5</p>
+				</div>
 				{/* <div className="absolute w-28 h-20 bg-blue-300 left-[9.2rem]">
 					<p>Player Number 4</p>
 				</div>
