83a81a3ac752c6f7714e57bbd3ac5eb5dcdf56ea
dchoi315 <dchoi315@umd.edu>
2024-01-15 20:12:03 -0500

    feat: actionbar display finished

--- packages/client/src/app/game/[roomId]/_stages/_components/actionBar.tsx
+++ packages/client/src/app/game/[roomId]/_stages/_components/actionBar.tsx
@@ -2,6 +2,8 @@
 
 import { Button } from "@/components/ui/button"
 import { useState } from "react"
+import { Slider } from "@/components/ui/slider"
+import { Plus, Minus } from "lucide-react"
 
 export const ActionBar = () => {
 	const [bidMenuOpen, setBidMenuOpen] = useState<boolean>(false)
@@ -11,15 +13,15 @@ export const ActionBar = () => {
 	}
 
 	return (
-		<div className="h-24 mx-auto w-full bg-red-200 absolute bottom-0 flex justify-between">
+		<div className="h-28 mx-auto w-full bg-red-200 absolute bottom-0 flex justify-between">
 			{!bidMenuOpen && (
 				<>
 					<div className="w-7/12 bg-blue-300">
 						<p>Cards</p>
 					</div>
 					<div className="w-5/12 flex justify-around items-center">
-						<Button className="h-2/3 lg:h-3/4 w-5/12">Pass</Button>
-						<Button onClick={handleBidClick} className="h-2/3 lg:h-3/4 w-5/12">
+						<Button className="m-0 p-0 h-2/3 lg:h-3/4 w-5/12">Pass</Button>
+						<Button onClick={handleBidClick} className="m-0 p-0 h-2/3 lg:h-3/4 w-5/12">
 							Bid
 						</Button>
 					</div>
@@ -28,12 +30,23 @@ export const ActionBar = () => {
 
 			{bidMenuOpen && (
 				<>
-					<div className="w-7/12 bg-blue-300">
-						<p>Menu</p>
+					<div className="w-7/12 bg-blue-300 p-4 flex justify-around">
+						<div className=" bg-green-500 p-4 ">
+							<p className="text-2xl text-center p-2">$14</p>
+						</div>
+						<div className="space-y-1 flex items-center flex-col lg:bg-pink-500 lg:flex-row w-3/5 lg:space-y-0">
+							<Button className="m-0 p-0 h-10 w-10 lg:rounded-r-none">
+								<Plus />
+							</Button>
+							<Slider className="hidden lg:inline-flex bg-gray-600 h-10 px-1" defaultValue={[33]} max={100} step={1} />
+							<Button className="m-0 p-0 h-10 w-10 lg:rounded-l-none">
+								<Minus />
+							</Button>
+						</div>
 					</div>
 					<div className="w-5/12 flex justify-around items-center">
-						<Button className="h-2/3 lg:h-3/4 w-5/12">Bid</Button>
-						<Button onClick={handleBidClick} className="h-2/3 lg:h-3/4 w-5/12">
+						<Button className="m-0 p-0 h-2/3 lg:h-3/4 w-5/12">Bid</Button>
+						<Button onClick={handleBidClick} className="m-0 p-0 h-2/3 lg:h-3/4 w-5/12">
 							Cancel
 						</Button>
 					</div>
--- packages/client/src/app/game/[roomId]/_stages/game.tsx
+++ packages/client/src/app/game/[roomId]/_stages/game.tsx
@@ -1,5 +1,6 @@
 import { GameState } from "../page"
 import { ActionBar } from "./_components/actionBar"
+import { AspectRatio } from "@/components/ui/aspect-ratio"
 
 interface GameProps {
 	gameState: GameState
@@ -7,7 +8,7 @@ interface GameProps {
 export const Game = ({ gameState }: GameProps) => {
 	return (
 		<div className="bg-green-200 h-screen max-w-screen-lg mx-auto relative overscroll-none">
-			<div className="relative h-[calc(100%-6rem)]">
+			<div className="relative h-[calc(100%-7em)]">
 				<div className=" absolute h-14 w-full bg-slate-400 top-[calc(50%-1.75rem)]">Community Cards</div>
 				<div className="absolute w-20 h-14 md:w-28 md:h-18 bg-blue-300 left-[calc(50%-2.5rem)] top-[0.5rem]">
 					<p>Player Number 4</p>
