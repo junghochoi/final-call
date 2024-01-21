5452b988b564a2068b172be5f8912ed29764337c
dchoi315 <dchoi315@umd.edu>
2024-01-21 00:32:00 -0500

    useEffect fix

--- packages/client/src/app/game/[roomId]/page.tsx
+++ packages/client/src/app/game/[roomId]/page.tsx
@@ -2,49 +2,17 @@
 
 import { useParams } from "next/navigation"
 import { useEffect, useState, useCallback } from "react"
-import { Socket } from "socket.io-client"
 
+import { Player, GameStateUpdatePayload, Stage, GameState, PlayerInit } from "@/types"
 import { getNickname, getSessionId, persistSessionId } from "@/lib/utils"
-import { getSocketConnection } from "@/lib/socketUtils"
+import { useSocket } from "@/contexts/SocketContext"
 import UsernameSelection from "./usernameSelection"
 import { Lobby, Game } from "./_stages"
-
-import { useSocket } from "@/contexts/SocketContext"
-
-import {
-	Player,
-	ServerToClientEvents,
-	ClientToServerEvents,
-	PlayerInitializationPayload,
-	GameStateUpdatePayload,
-	Stage,
-	Action,
-	ClientBidState,
-	GameState,
-	PlayerInit,
-} from "@/types"
-
-// let socket: Socket<ServerToClientEvents, ClientToServerEvents>
-
-// async function initializeSocket(roomId: string) {
-// 	socket = await getSocketConnection(roomId)
-// }
-
-function useNickname() {
-	const [nickname, setNickname] = useState<string | undefined>()
-	useEffect(() => {
-		setNickname(getNickname())
-	}, [])
-	return nickname
-}
 
 const GamePage = () => {
 	const { socket } = useSocket()
 	const { roomId } = useParams<{ roomId: string }>()
-	// const nickname = useNickname()
-
-	const [name, setName] = useState<string | undefined>(getNickname())
-
+	const [pickedName, setPickedName] = useState<boolean>(getNickname() !== undefined)
 	const [connected, setConnected] = useState<boolean>(false)
 	const [gameState, setGameState] = useState<GameState>({
 		stage: Stage.Lobby,
@@ -66,43 +34,28 @@ const GamePage = () => {
 	}, [])
 
 	useEffect(() => {
-		socket.on("connect", () => {
-			startGameEventHandlers()
+		startGameEventHandlers()
+	}, [])
 
-			if (name !== undefined) {
-				const playerInit: PlayerInit = {
-					nickname: name,
-					roomId,
-					sessionId: getSessionId(),
-					host: true,
-					socketId: socket.id!,
-				}
-				socket.emit("PlayerInitialization", playerInit, playerInitializationCallback)
+	useEffect(() => {
+		socket.on("connect", () => {
+			if (!pickedName) return
+
+			const playerInit: PlayerInit = {
+				nickname: getNickname() as string,
+				roomId,
+				sessionId: getSessionId(),
+				host: true,
+				socketId: socket.id!,
 			}
+			socket.emit("PlayerInitialization", playerInit, playerInitializationCallback)
 		})
 
 		return () => {
 			socket.off("connect")
 		}
-	}, [name])
+	}, [pickedName])
 
-	const handleUserJoinGame = () => {
-		const sessionId = getSessionId()
-		const nickname = getNickname()
-		setName(nickname)
-
-		const player: PlayerInit = {
-			nickname: getNickname() as string,
-			roomId,
-			sessionId,
-			host: false,
-			socketId: socket.id!,
-		}
-		socket.emit("PlayerInitialization", player, playerInitializationCallback)
-	}
-	const handleStartGame = () => {
-		socket.emit("StageChange", { roomId: roomId, stage: Stage.Bidding })
-	}
 	const startGameEventHandlers = () => {
 		socket.on("GameStateUpdate", (gameStateUpdate: GameStateUpdatePayload) => {
 			const currPlayer = gameStateUpdate.players.find((player) => player.sessionId === getSessionId())
@@ -114,7 +67,23 @@ const GamePage = () => {
 		})
 	}
 
-	if (name === undefined) {
+	const handleUserJoinGame = (name: string) => {
+		setPickedName(true)
+		const sessionId = getSessionId()
+		const player: PlayerInit = {
+			nickname: name,
+			roomId,
+			sessionId,
+			host: false,
+			socketId: socket.id!,
+		}
+		socket.emit("PlayerInitialization", player, playerInitializationCallback)
+	}
+	const handleStartGame = () => {
+		socket.emit("StageChange", { roomId: roomId, stage: Stage.Bidding })
+	}
+
+	if (!pickedName) {
 		return <UsernameSelection handleUserJoinGame={handleUserJoinGame} />
 	} else if (gameState.stage == Stage.Lobby) {
 		return <Lobby gameState={gameState} handleStartGame={handleStartGame} />
